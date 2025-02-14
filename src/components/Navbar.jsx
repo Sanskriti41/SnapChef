2d155acb43c67deaf0a115348e47dadfe0f89668
Sanskriti <sanskriti761@gmail.com>
2025-02-15 02:09:24 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <nav>
+            
+        </nav>
+    );
+};
+
+export default Navbar;
